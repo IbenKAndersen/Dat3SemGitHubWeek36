18eba958f16cd6936a60d9da18981741463ae1e0
Iben A <cph-ia62@cphbusiness.dk>
2019-09-06 09:12:48 +0200

    Fetch html + js

---
+++ 04_thursday_exercises/script.js
@@ -0,0 +1,9 @@
+fetch(https://jsonplaceholder.typicode.com/users)
+  .then(res => res.json()) //in flow1, just do it
+  .then(data => {
+   // Inside this callback, and only here, the response data is available
+    console.log("data",data);
+  /* data now contains the response, converted to JavaScript
+     Observe the output from the log-output above
+     Now, just build your DOM changes using the data      */ 
+})
